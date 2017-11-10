5a2bef1280d1a3e735af66e9fafbafe3b4083264
Richard Xu <xxu@huskers.unl.edu>
2017-11-10 11:47:25 -0600

    add setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='pyunlvrtm',
+      version='0.1',
+      description='Python packages to faciliate the UNL-VRTM model',
+      url='https://github.com/xxu2/pyunlvrtm',
+      author='Richard Xu',
+      author_email='xxu@huksers.unl.edu',
+      license='MIT',
+      packages=['pyunlvrtm'],
+      zip_safe=False)
